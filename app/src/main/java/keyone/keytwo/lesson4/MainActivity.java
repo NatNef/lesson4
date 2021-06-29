122228e214a89c3628cd7e6082f1f23e5060fbb1
Natalia <nef.n@list.ru>
2021-06-29 14:31:33 +0300

    Создали папку assets через файловый менеджер.

--- app/src/main/java/keyone/keytwo/lesson4/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson4/MainActivity.java
@@ -2,7 +2,10 @@ package keyone.keytwo.lesson4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +14,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Ресурсы
+
         //получить в коде ресурс язык
         String myString = getResources().getString(R.string.hello_text);
         //обратились к ресурсам-просим вернуть строку-указываем стринг(или id
+
+
+        // подключить шрифт
+        Typeface tf = Typeface.createFromAsset(getAssets(),"fonts/19659.ttf");
+        // далее задаем элементу id и находим его по нему
+        TextView textView = findViewById(R.id.textView);
+        // далее применем к нему фон
+        textView.setTypeface(tf);
+
+
+        // можно програмно задать текст
+        textView.setText(myString);
+
+        //подгрузить изображение
+        Drawable draw = getAssets().open("android.png");
+// просим ресурс менеджера вернуть нам любой файл и он будет считываться как потом байтов
+        // можем работать с любыми изображениями
+        //менять текс, обрезать, и тд
+
+
+        // Стили
+
+        
+
+
     }
 
 }
